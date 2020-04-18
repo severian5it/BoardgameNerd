43446c60b25fa60d55ff62475bca3ae607c0d447
Pierluca Del Buono <pierluca@amazon.com>
2020-04-18 18:21:46 +0200

    adding login page and method

--- app.py
+++ app.py
@@ -17,6 +17,26 @@ def index():
     docs=doc["items"]["item"]
     return render_template("pages/index.html",  docs=docs, loggedIn=loggedIn)
 
+# login page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    loggedIn = True if 'user' in session else False
+
+    if loggedIn == True:
+        user_in_db = mongo.db.BoardGame.users.find_one({"username": session['user']})
+        if user_in_db:
+            return redirect(url_for('my_account_page', username=user_in_db['username']))
+
+    if request.method == 'POST':
+        post_request = request.get_json()
+        # response = login_req(db, post_request)
+        # return json.dumps(response)
+
+    return render_template(
+        "pages/login.html",
+        loggedIn=loggedIn,
+    )
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
