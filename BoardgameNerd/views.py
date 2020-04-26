60f8bc89c2ca400140d8ac064329639501849a8f
Pierluca Del Buono <pierluca@amazon.com>
2020-04-26 13:37:50 +0200

    including handlers for 404 and 500

--- BoardgameNerd/views.py
+++ BoardgameNerd/views.py
@@ -146,4 +146,14 @@ def settings():
             user=user
         )
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_erro(e):
+    # note that we set the 500 status explicitly
+    return render_template('500.html'), 500
+
 
