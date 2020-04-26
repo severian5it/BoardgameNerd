d6e3e0dfabcae4dfd0f25ea067769c9f63878a29
Pierluca Del Buono <pierluca@amazon.com>
2020-04-26 13:32:18 +0200

    changing login code to cover corner case

--- BoardgameNerd/helper/form.py
+++ BoardgameNerd/helper/form.py
@@ -2,16 +2,16 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flask import session
 
 def check_user_login(db, post_request):
-    user = db.users.find_one({'$or': [{ 'username': post_request['username']}, 
-                                {'email': post_request['email'] } ]})
+    username = post_request.get('username')
+    password = post_request.get('password')
+    user = db.users.find_one({'$or': [{ 'username': username}, 
+                                {'email': username} ]})
     passwordCorrect = False
-    username = ''
 
     if user: 
-        if check_password_hash(user['password'], post_request['password']):
+        if check_password_hash(user['password'], password):
             session['user'] = user['username']
             passwordCorrect = True    
-            username = user['username']    
 
     response = {
         "userMatch": True if user else False,
