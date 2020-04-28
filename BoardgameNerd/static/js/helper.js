a2a858fbed6d66450e3ccc0f45f2d711038bba87
Pierluca Del Buono <pierluca@amazon.com>
2020-04-28 19:09:54 +0200

    adding multi-item carousel on landing page

--- BoardgameNerd/static/js/helper.js
+++ BoardgameNerd/static/js/helper.js
@@ -14,4 +14,26 @@ $(document).ready(function(){
         $('.searchButton').submit()
     });
 
+    $('#hotCarousel').carousel({
+      interval: 5000
+    })
+    
+    $('.carousel .carousel-item').each(function(){
+        var minPerSlide = 3;
+        var next = $(this).next();
+        if (!next.length) {
+        next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+        
+        for (var i=0;i<minPerSlide;i++) {
+            next=next.next();
+            if (!next.length) {
+              next = $(this).siblings(':first');
+            }
+            
+            next.children(':first-child').clone().appendTo($(this));
+          }
+    });
+
 });
