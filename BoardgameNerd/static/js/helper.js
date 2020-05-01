ba5e523acddc68e80f1505569d6968456fdaa8f8
Pierluca Del Buono <pierluca@amazon.com>
2020-05-01 14:55:01 +0200

    changed code for carousel bootstrap multi item to solve past issue

--- BoardgameNerd/static/js/helper.js
+++ BoardgameNerd/static/js/helper.js
@@ -14,28 +14,32 @@ $(document).ready(function(){
         $('.searchButton').submit()
     });
 
-    $('#hotCarousel').carousel({
-      interval: 5000
-    })
     
-    $('.carousel .carousel-item').each(function(){
-        var minPerSlide = 3;
-        var next = $(this).next();
-        if (!next.length) {
-        next = $(this).siblings(':first');
-        }
-        next.children(':first-child').clone().appendTo($(this));
-        
-        for (var i=0;i<minPerSlide;i++) {
-            next=next.next();
-            if (!next.length) {
-              next = $(this).siblings(':first');
-            }
-            
-            next.children(':first-child').clone().appendTo($(this));
-          }
-    });
+/*
+   code for carousel from here:  https://azmind.com/bootstrap-carousel-multiple-items 
 
-    $(".prova").attr('src', "/thumbnail/82298");
+*/
+$('#carousel-example').on('slide.bs.carousel', function (e) {
+  /*
+      CC 2.0 License Iatek LLC 2018 - Attribution required
+  */
+  var $e = $(e.relatedTarget);
+  var idx = $e.index();
+  var itemsPerSlide = 5;
+  var totalItems = $('.carousel-item').length;
+
+  if (idx >= totalItems-(itemsPerSlide-1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i=0; i<it; i++) {
+          // append slides to end
+          if (e.direction=="left") {
+              $('.carousel-item').eq(i).appendTo('.carousel-inner');
+          }
+          else {
+              $('.carousel-item').eq(0).appendTo('.carousel-inner');
+          }
+      }
+  }
+});
 
 });
